cf150c86c8335a2a78f97cf4f71dc3e48cec97a2
Julie Pan <ycp217@nyu.edu>
2015-04-18 23:44:00 -0400

    comment on code for dumbos

--- shop/spiders/forever21.py
+++ shop/spiders/forever21.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
 from shop.items import ShopItem
 
+# scraper for any product page on forever21 (works ONLY for f21)
 class Forever21Spider(scrapy.Spider):
     name = "forever21"
     allowed_domains = ["forever21.com"]
+
+    # replace start_url with product page
     start_urls = ('http://www.forever21.com/Product/Product.aspx?Br=F21&Category=DRESS&ProductID=2000134198&VariantID=&recid=product_rr-_-2002247963-_-2000134198-_-613-_-1',
     )
 
