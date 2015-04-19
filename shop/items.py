5e71f94d2e804496c9cf79cb65ef0b75ffc1bc76
Julie Pan <ycp217@nyu.edu>
2015-04-18 23:41:52 -0400

    scrapes imageUrl, title, price

--- shop/items.py
+++ shop/items.py
@@ -12,5 +12,5 @@ class ShopItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     title = scrapy.Field()
-    link = scrapy.Field()
-    desc = scrapy.Field()
+    price = scrapy.Field()
+    imageUrl = scrapy.Field()
--- shop/spiders/forever21.py
+++ shop/spiders/forever21.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+from shop.items import ShopItem
 
 class Forever21Spider(scrapy.Spider):
     name = "forever21"
     allowed_domains = ["forever21.com"]
-    start_urls = ('http://www.forever21.com/Product/Product.aspx?BR=f21&Category=dress&ProductID=2002247963&VariantID=',
+    start_urls = ('http://www.forever21.com/Product/Product.aspx?Br=F21&Category=DRESS&ProductID=2000134198&VariantID=&recid=product_rr-_-2002247963-_-2000134198-_-613-_-1',
     )
+
 # class = "product-title"
     def parse(self, response):
-        title = response.xpath('//title').extract()
-        print title
-        
+        item = ShopItem()
+        item['title'] = response.xpath('//*[@class="product-title"]/text()').extract()
+        item['price'] = response.xpath('//*[@class="product-price"]/text()').extract()
+        # item['imageUrl'] = response.xpath('//*[@id="ctl00_MainContent_productImage"]').extract()
+        # response.css('img').xpath('@src').extract()
+        item['imageUrl'] = response.xpath('//*[@id="ctl00_MainContent_productImage"]/@src').extract()
+
+        yield item 
